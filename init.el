c12521277865aa6b31a4504be77805cd536ed392
Paul Richardson <paulrichardson@attask.com>
2014-12-15 10:56:43 -0700

    Clean up

--- init.el
+++ init.el
@@ -21,156 +21,49 @@
 (if window-system (set-exec-path-from-shell-PATH))
 
 (require 'package)
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-  '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("ELPA" . "http://tromey.com/elpa/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")
+        ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(setq stable-packages
+  '(cider
+    magit
+    multiple-cursors
+    projectile))
+
+(setq package-enable-at-startup nil)
 (package-initialize)
 
-(setq my-packages
-  '(magit
-;    expand-region
-; descbinds-anything
-; goto-last-change
-; gnus
-; bbdb
-; pgdevenv-el
-; mbsync
-; smex
-; geiser
- multiple-cursors
-; anything
-; pcmpl-git
-; magit-view-file
-; emacs-goodies-el
-; sicp
-; auto-dictionnary
-; keywiz
-; pandoc-mode
-; psql-linum-format
-; psvn
-; rect-mark
-; crontab-mode
-; incomplete+
-; php-mode-improved
-; rainbow-delimiters
-; muse
-; deft
-; dpans2texi
-; markdown-mode
-; color-theme-solarized
-; protobuf-mode
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
-    ;; cssh
-    ;; asciidoc
-    ;; lisppaste
-    better-defaults
-    ;; switch-window
-    ;; vkill
-    ;; google-maps
-    ;; nxhtml
-    ;; xcscope
-    ;; yasnippet
-    ;; escreen
-    ;; switch-window
-    auto-complete
-    ;; zencoding-mode
-    ;; color-theme
-    ;; color-theme-tango
-    paredit
+(setq other-packages
+      '(better-defaults . "melpa"))
 
-; clj-refator
-; idle-highlight-mode
-; smeargle
-; quickrun
-; restclient
-; sqlup
-; paradox
-; git-timemachine
-; diff-hl (or h1)
-; agda2-mode
+(setq package-pinned-packages
+      (let ((cons-stable (lambda (p) (cons p "melpa-stable"))))
+        (mapcar cons-stable stable-packages)))
 
-; LOST -------
-; saveplace
-; malabar-mode
+(projectile-global-mode)
 
- projectile
-; wgrep
-; ido-ubiquitous
-; ac-nrepl
-; ace-jump-mode
-; emmet-mode
-; etags-select
-; expand-region
-; fill-column-indicator
-; flycheck-hdevtools
-; fuzzy
-; ghc-mod
-; ghci-completion
-; gist
-; go-mode
-; guru-mode
-; highlight-parentheses
-; highlight-symbol
-; hl-sexp
-; idle-highlight-mode
-; twittering-mode
-; ido-menu
-; inf-ruby
-; key-chord
-; nav
-; robe-mode
-; undo-tree
-; yasnippet
-; color-theme-tomorrow
-; col-highlight
-; fuzzy-match
-; iy-go-to-char
-; mic-paren
-; tagedit
-
-; exec-path-from-shell
-; clojure-mode-extra-font-locking
-; company
-; ack-and-a-half
-; modes: scss coffee yaml enh-ruby
-; ag
-
-; buffer-move
-    cider))
-
-(if (not (package-installed-p 'package-filter))
-    (progn
-      (switch-to-buffer
-       (url-retrieve-synchronously
-        "https://raw.github.com/milkypostman/package-filter/master/package-filter.el"))
-      (package-install-from-buffer  (package-buffer-info) 'single)))
-
-(setq package-archive-enable-alist
-      '(("melpa" better-defaults)))
-
-(package-refresh-contents)
-
-(dolist (p my-packages)
-  (if (not (package-installed-p p))
-      (package-install p)))
-
-(autoload 'enable-paredit-mode "paredit" nil t)
-(add-hook 'emacs-list-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'clojure-mode-hook #'enable-paredit-mode)
+(require 'smartparens-config)
+(smartparens-global-mode t)
 
 (require 'whitespace)
 (setq whitespace-style '(empty tabs trailing lines-tail face))
-(setq whitespace-line-column 120)
+(setq whitespace-line-column 125)
 (global-whitespace-mode t)
+
+(setq c-basic-offset 2)     ;; how far to indent
+(setq tab-width 4)          ;; interpretation of ascii 9
+(setq indent-tabs-mode nil) ;; don't use ascii byte #9
 
 (setq frame-title-format '(:eval (if (buffer-file-name) "%b — %f" "%b")))
 
-; use forward symbol instead of forward-word
-; key bindings
-
-(setq max-specpdl-size 10)
+;(setq max-specpdl-size 10) ; limit stack size in elisp
 (setq debug-on-error t)
 
 (setq mac-command-modifier 'meta)
