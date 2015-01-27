ac383acb45c89b2dd6c9370af6b157a8ef7cde2b
Paul Richardson <paulrichardson@attask.com>
2015-01-27 09:44:25 -0700

    Restructure to contain more config than only emacs
     * Add lein config: profiles and plzmap
     * TODO: Add bash profile to share - or at least a base

---
+++ lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[cider/cider-nrepl "0.8.1"]
+                  [lein-ancient "0.5.5"]
+                  [lein-plz "0.3.0" :exclusions [[rewrite-clj] [ancient-clj]]]]
+        :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]]
+        :plz ["/Users/paulrichardson/.lein/plzmap.edn"]}}
