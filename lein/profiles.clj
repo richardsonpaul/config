0c50326c71cf723afebb3932fa28fb22c502f4aa
Paul Richardson <paulrichardson@attask.com>
2015-05-19 16:21:00 -0600

    Update lein-plz version to fix lein ancient

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [refactor-nrepl "0.2.2"]
                   [lein-ancient "0.6.1"]
-                  [lein-plz "0.3.1" :exclusions [[rewrite-clj] [ancient-clj]]]]
+                  [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]]
         :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]]
         :plz ["/Users/paulrichardson/.lein/plzmap.edn"]}}
