6e8003c2853bb95b918da10f3827ae357958e7a4
Paul Richardson <paulrichardson@attask.com>
2015-06-16 16:27:10 -0600

    Fix some cider keymappings, update cider version

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0"]
                   [refactor-nrepl "1.0.5"]
                   [lein-ancient "0.6.7"]
                   [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]]
