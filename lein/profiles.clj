4cb43665941f358862c75c46de18de39e024bc87
Paul Richardson <paul@frittata>
2015-10-31 21:03:13 -0600

    Bad changes by lein ancient, broke stuff

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0"]
-                  [refactor-nrepl "1.0.5"]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.2.0" :exclusions [org.clojure/tools.nrepl]]
                   [lein-ancient "0.6.7"]
                   [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]]
         :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                       [org.clojure/tools.nrepl "0.2.10"]]
+                       ^:replace "0.2.12"]
         :plz ["/Users/paulrichardson/.lein/plzmap.edn"]}}
