4f668ca0ed5c2ff6e6ef6d73c7fb4a6260011702
Paul Richardson <paul@frittata>
2015-10-31 21:05:12 -0600

    Fix stuff lein ancient broke

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,5 +3,5 @@
                   [lein-ancient "0.6.7"]
                   [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]]
         :dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
-                       ^:replace "0.2.12"]
+                       [org.clojure/tools.nrepl "0.2.12"]]
         :plz ["/Users/paulrichardson/.lein/plzmap.edn"]}}
